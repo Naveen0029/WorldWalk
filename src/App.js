89422138d9ddf89530cb3340c032b5bec45c87e4
Naveen0029 <naveendharnia0029@gmail.com>
2023-04-13 23:30:53 +0530

    added map

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from './pages/Home';
-import ContactUs from './pages/ContactUs';
+import ContactsUs from './pages/ContactsUs';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/contact-us' element={<ContactUs />} />
+        <Route path='/contact-us' element={<ContactsUs />} />
       </Routes>
       </BrowserRouter>
       <Footer />
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import GoogleMapReact from 'google-map-react';
+import './Map.css'
 
 const Map = () => {
   return (
     <div className='Map'>
-        <GoogleMapReact
-        bootstrapURLKeys={{ key: 'key' }}
-        defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
-        defaultZoom={10}
-      >
-        {/* You can add markers, polygons, and other elements here */}
-      </GoogleMapReact>
+        <iframe
+          title="My Google Map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3471.2819031568806!2d75.70789747628992!3d29.53728367518296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3911797bd916e4cd%3A0x8b0ddf66e07fc2cd!2sWorld%20walk!5e0!3m2!1sen!2sin!4v1681407830337!5m2!1sen!2sin"
+          width="100%"
+          height="100%"
+          allowFullScreen=""
+          loading="lazy"
+        ></iframe>
     </div>
+    //<iframe src= width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   )
 }
 
