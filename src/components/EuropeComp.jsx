c62042f38d30cf8864f49f9d30c7c93a7232f80f
Naveen0029 <naveendharnia0029@gmail.com>
2023-06-09 01:34:54 +0530

    our services

--- src/components/EuropeComp.jsx
+++ src/components/EuropeComp.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./EuropeComp.css";
 import Poland from '../img/Poland.png'
 import malta from '../img/malta.png'
+import carotia from'../img/carotia.jpg'
+import portugal from '../img/portugal.png'
 
 const EuropeComp = () => {
   return (
@@ -36,6 +38,54 @@ const EuropeComp = () => {
         </div>
         <div>
           <h3>MALTA</h3>
+        </div>
+        <div>
+          <p>Study Visa</p>
+        </div>
+        <div>
+          <p>Work Visa</p>
+        </div>
+        <div>
+          <p>Business Visa</p>
+        </div>
+        <div>
+          <p>PR Visa - Express Entry</p>
+        </div>
+        <div>
+          <p>PR Visa - PNP</p>
+        </div>
+      </div>
+
+      <div className="cards">
+        <div>
+          <img alt="Malta" src={carotia} className="country" style={{"border":"1px solid black"}} />
+        </div>
+        <div>
+          <h3>CAROTIA</h3>
+        </div>
+        <div>
+          <p>Study Visa</p>
+        </div>
+        <div>
+          <p>Work Visa</p>
+        </div>
+        <div>
+          <p>Business Visa</p>
+        </div>
+        <div>
+          <p>PR Visa - Express Entry</p>
+        </div>
+        <div>
+          <p>PR Visa - PNP</p>
+        </div>
+      </div>
+
+      <div className="cards">
+        <div>
+          <img alt="Malta" src={portugal} className="country" style={{"border":"1px solid black"}} />
+        </div>
+        <div>
+          <h3>PORTUGAL</h3>
         </div>
         <div>
           <p>Study Visa</p>
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './Service.css'
+import passport from '../../src/img/passport.png'
+import moneyTransfer from '../../src/img/bank-transfer.png'
+import toursAndTravel from '../../src/img/flight.png'
 
 const Service = () => {
   return (
+    <div className='serviceHeader'>
+    <h1>Our Services</h1>  
     <div className='Service'>
       <div className='Servicecards'>
+        <div><img alt="Immigration" src={passport} className="country" /></div>
+        <div className='cardHeader'><h5>Immigration</h5></div>
       </div>
+      <div className='Servicecards'>
+        <div><img alt="Immigration" src={moneyTransfer} className="country" /></div>
+        <div className='cardHeader'><h5>Money Transfer</h5></div>
+      </div>
+      <div className='Servicecards'>
+        <div><img alt="Immigration" src={toursAndTravel} className="country" /></div>
+        <div className='cardHeader'><h5>Tours & Travels</h5></div>
+      </div>
+    </div>
     </div>
   )
 }
